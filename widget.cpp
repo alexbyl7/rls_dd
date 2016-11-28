a5abbac237c9aec0d30fc538c758a1027b57b826
byldin <byldin@mail.ru>
2016-11-28 12:35:48 +0300

    Добавил управление коэффициентами, масштабом и проч.

--- widget.cpp
+++ widget.cpp
@@ -15,9 +15,9 @@ Widget::Widget(Parser *pars) :
   pixmap->fill(Qt::white);
   length = qMin(pixmap->width(), pixmap->height()) / 3;
 
-  timer = new QTimer(this);
-  connect(timer, SIGNAL(timeout()), SLOT(paintEvent()));
-  timer->start(1000);
+  //timer = new QTimer(this);
+  //connect(timer, SIGNAL(timeout()), SLOT(update()));
+  //timer->start(1000);
 }
 
 Widget::~Widget()
@@ -32,23 +32,25 @@ void Widget::paintEvent(QPaintEvent*) {
   painter.begin(pixmap);
   painter.setRenderHint(QPainter::Antialiasing);
   painter.translate(pixmap->width() / 2, pixmap->height() / 2);
-  double s = 0.1;
+
+  double s = ui->doubleSpinBox_scale->value();
   painter.scale(s, s);
+  double line_width = ui->spinBox_linewidth->value();
 
   DATA_PACKAGE_AD data = parser->getData();
   painter.rotate(360.0 * data.data.line_pos.pos / 4096);
 
   // Для сжатия:
-  double a = 50,
-         b = 0,
-         c = 0.4;
+  double a = (double)ui->horizontalSlider_A->value() / 100,
+         b = (double)ui->horizontalSlider_B->value(),
+         c = (double)ui->horizontalSlider_C->value() / 100;
 
   int step = 1;
   for (int i = 0; i < DATA_LEN_SPECTR_4K_16B; i++) {
     float x = data.data.out_data.spectr[i];
     int col = pow(a*x+b, c);
     if (col > 255) col = 255;
-    painter.setPen( QPen(QColor(col,col,col), 30.0, Qt::SolidLine) );
+    painter.setPen( QPen(QColor(col,col,col), line_width, Qt::SolidLine) );
     painter.drawLine(i*step,i*step, (i+1)*step,(i+1)*step);
   }
 
